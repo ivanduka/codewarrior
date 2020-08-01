083d4b1f4bf9906f0693c554ac16dc1ff8ae46fe
Ivan Duka <ivan.duka.pm@gmail.com>
2020-07-31 20:54:37 -0600

    works with waiting for all

--- kata.go
+++ kata.go
@@ -5,20 +5,57 @@ import (
 	"encoding/hex"
 	"fmt"
 	"runtime"
+	"strconv"
 )
 
-const maximum = 99999999
+type crackingUnit struct {
+	start   int
+	end     int
+	padding int
+	target  string
+	result  chan<- string
+}
 
-func Crack(hash string) string {
-	for i := 0; i <= maximum; i += 1 {
-		paddedString := fmt.Sprintf("%05d", i)
+func crackPart(cu crackingUnit) {
+	for i := cu.start; i <= cu.end; i += 1 {
+		paddedString := fmt.Sprintf("%0*d", cu.padding, i)
 		currentHash := md5.Sum([]byte(paddedString))
 		currentHashString := hex.EncodeToString(currentHash[:])
-		if currentHashString == hash {
-			return paddedString
+		if currentHashString == cu.target {
+			cu.result <- paddedString
+			return
 		}
 	}
-	return ""
+	cu.result <- ""
+}
+
+const maximum = 9999999
+
+func Crack(hash string) string {
+	workUnits := divideIntegers()
+	result := make(chan string)
+	defer close(result)
+	padding := len(strconv.Itoa(maximum))
+
+	for _, pair := range workUnits {
+		cu := crackingUnit{
+			start:   pair[0],
+			end:     pair[1],
+			padding: padding,
+			target:  hash,
+			result:  result,
+		}
+		go crackPart(cu)
+	}
+
+	var correctAnswer string
+	for range workUnits {
+		data := <-result
+		if data != "" {
+			correctAnswer = data
+		}
+	}
+	return correctAnswer
 }
 
 func md5Hash(s string) string {
@@ -26,8 +63,25 @@ func md5Hash(s string) string {
 	return hex.EncodeToString(currentHash[:])
 }
 
+func divideIntegers() [][2]int {
+	numCPU := runtime.NumCPU()
+	//numCPU := 1
+	slice := make([][2]int, numCPU)
+	division := maximum / numCPU
+	lastStart := -1
+	for index := range slice {
+		slice[index][0] = lastStart + 1
+		if index == len(slice)-1 {
+			slice[index][1] = maximum
+		} else {
+			slice[index][1] = lastStart + 1 + division
+			lastStart += division + 1
+		}
+	}
+	return slice
+}
+
 func main() {
-	s := md5Hash("99999999")
+	s := md5Hash("9999999")
 	fmt.Println(s)
-	fmt.Println(runtime.NumCPU())
 }
